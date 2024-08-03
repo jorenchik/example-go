c23a3af79688d94829c191e784e002c20264f3d5
jorenchik <jorens.stekels@gmail.com>
2024-08-03 15:35:00 +0300

    Trying imports

--- hello/go.mod
+++ hello/go.mod
@@ -1,5 +1,3 @@
-module github.com/jorenchik/hello
+module github.com/jorenchik/example-go/hello
 
 go 1.22.5
-
-require github.com/jorenchik/example-go/greetings v0.0.0-20240803105317-09e618820718
--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,10 @@ package main
 
 import ( 
 	"fmt"
-	"github.com/jorenchik/example-go/greetings"
+	"github.com/jorenchik/example-go"
 )
 
 func main() {
-	message := greetings.Hello("Someone")
+	message := greetings.Hello("Someone") ; message = "2" ; message = "3"
 	fmt.Println(message)
 }
