10075145bb7b649dc048675adcf4ab8a74ef43ed
jorenchik <jorens.stekels@gmail.com>
2024-08-03 16:06:36 +0300

    someht

---
+++ hello/go.mod
@@ -0,0 +1,3 @@
+module github.com/jorenchik/example-go/hello
+
+go 1.22.5
---
+++ hello/hello.go
@@ -0,0 +1,7 @@
+package hello
+
+import "fmt"
+
+func Hello(name string) {
+	fmt.Println(fmt.Sprintf("Hello, %s! Great to see you!", name))
+}
